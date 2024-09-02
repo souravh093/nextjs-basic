e13d281b4cf20084521146d69be2693a85a41620
Sourave Halder <souravh093@gmail.com>
2024-09-02 19:45:58 +0600

    make the image system

---
+++ src/app/gallery/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GalleryPage = () => {
+  return (
+    <div>
+        <img src="https://i18nexus.com/_next/static/media/nextjs.e54be70c.svg" alt='image' className='w-44 h-44 mx-auto' />
+    </div>
+  )
+}
+
+export default GalleryPage
